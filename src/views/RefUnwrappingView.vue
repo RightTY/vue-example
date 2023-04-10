cc7f30a0a419f4c964582932d4719fc3883f5746
jack.shi <jack.shi@revival.com>
2023-04-10 14:32:49 +0800

    vue Ref Unwrapping Example

---
+++ src/views/RefUnwrappingView.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+import { ref } from "vue";
+
+const object = { foo: ref(1), too: ref(2) };
+const { foo } = object;
+const { too } = object;
+function increment() {
+  foo.value++;
+}
+function increment1() {
+  too.value++;
+}
+</script>
+
+<template>
+  <button @click="increment">{{ object.foo.value + 1 }}</button>
+  <button @click="increment">{{ foo + 1 }}</button>
+  <button @click="increment1">{{ too + 1 }}</button>
+</template>
